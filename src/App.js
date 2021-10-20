d2a1619d8997859013e6e94093fb30203bbc3761
devu <9919103182@mail.jiit.ac.in>
2021-10-20 18:52:01 +0530

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import logo from './logo.svg';
-import Grid from './Components/Grid'
-import HelpAbout from './Components/Help-About'
-import './Style.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Grid from "./Components/Grid";
+import HelpAbout from "./Components/Help-About";
+import "./Style.css";
 
 function App() {
-  return (
-    <div className="container">
-          {/* <h3 className="logo">Pathfinder</h3> */}
-          {/* <p className="subTitle">A visualization of different pathfinding algorithms.</p> */}
-          {/* <HelpAbout /> */}
-          <Grid />
-    </div>
-  );
+	return (
+		<div className="container">
+			{/* <h3 className="logo">Pathfinder</h3> */}
+			{/* <p className="subTitle">A visualization of different pathfinding algorithms.</p> */}
+			{/* <HelpAbout /> */}
+			<Grid />
+		</div>
+	);
 }
 
 export default App;
